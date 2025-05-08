3509637964f0a98a8c60b945aff0c0f88da76a46
poke6402j <poke6402j@gmail.com>
2025-05-08 14:28:06 +0800

    HelloWorldCommand

--- HelloWorld/HelloWorld.cs
+++ HelloWorld/HelloWorld.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 
 namespace HelloWorld
 {
+    [Transaction(TransactionMode.Manual)]
     public class HelloWorld : IExternalCommand
     {
         public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
